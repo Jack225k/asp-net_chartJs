5dddd2f81fdf267045c782042d41fc1520593c45
kaybo <kotzecabo@live.com>
2018-09-06 17:30:30 +0200

    Initial Commit

--- asp.net chart.js app/Controllers/HomeController.cs
+++ asp.net chart.js app/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using asp.net_chart.js_app.Models;
+using System.Data;
+using System.Threading;
 
 namespace asp.net_chart.js_app.Controllers
 {
@@ -38,6 +40,45 @@ namespace asp.net_chart.js_app.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public JsonResult NewChart()
+        {
+            List<object> iData = new List<object>();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Employee", System.Type.GetType("System.String"));
+            dt.Columns.Add("Credit", System.Type.GetType("System.Int32"));
+
+
+            DataRow dr = dt.NewRow();
+            dr["Employee"] = "Sam";
+            dr["Credit"] = 400;
+                
+            
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["Employee"] = "Alex";
+            dr["Credit"] = 456;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["Employee"] = "Michael";
+            dr["Credit"] = 587;
+            dt.Rows.Add(dr);
+
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                iData.Add(x);
+            }
+
+            return Json(iData);
+
+
         }
     }
 }
