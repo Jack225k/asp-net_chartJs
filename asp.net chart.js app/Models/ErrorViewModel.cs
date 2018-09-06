db6c90bd2b36272659e044083cfa22d539d461b0
kaybo <kotzecabo@live.com>
2018-09-06 16:06:21 +0200

    Add project files.

---
+++ asp.net chart.js app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp.net_chart.js_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
